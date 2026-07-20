21db41b2a2d7072bd82dabf6c07234abcd92baf2
runsenwu <84985987+runsenwu@users.noreply.github.com>
2026-07-19 21:19:11 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::fs::{self, Permissions};
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::os::unix::fs::PermissionsExt;
+use std::path::Path;
 use std::{self, collections::HashSet};
+use std::{env, path, string};
 
 fn main() {
     loop {
@@ -18,6 +22,8 @@ fn main() {
 
         let commands = HashSet::from(["echo", "exit", "type"]);
 
+        let dirs: Vec<&str> = vec![];
+
         // I think all of these could be stored within a type of some kind as a switch case
         // to match against
         if input == "exit" {
@@ -25,11 +31,36 @@ fn main() {
         } else if input.starts_with("echo ") {
             println!("{}", &input[5..]);
         } else if input.starts_with("type ") {
-            let command = &input[5..];
+            let command = &input[5..].trim();
             if commands.contains(command) {
                 println!("{command} is a shell builtin");
             } else {
-                println!("{command}: not found")
+                let test = env::var("PATH").unwrap_or_default();
+
+                if test.is_empty() {
+                    println!("{command}: not found")
+                } else {
+                    let paths = env::split_paths(&test);
+
+                    // perm checking
+                    for path in paths {
+                        let path_with_file = Path::new("{path}/{command}");
+                        if path_with_file.exists() {
+                            let permission = match fs::metadata(path) {
+                                Ok(mode) => mode.permissions().mode(),
+                                Err(_) => continue,
+                            };
+
+                            // if executable
+                            if permission & 0o111 != 0 {
+                                println!(
+                                    "{command} is {}",
+                                    path_with_file.to_str().unwrap_or_default()
+                                );
+                            }
+                        }
+                    }
+                }
             }
         } else {
             println!("{input}: command not found");
