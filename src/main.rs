f20553ad057c6f2d20eddf61abf78e70ae463b28
runsenwu <84985987+runsenwu@users.noreply.github.com>
2026-07-19 19:59:06 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::{self, collections::HashSet};
 
 fn main() {
     loop {
@@ -16,10 +16,19 @@ fn main() {
 
         let input = input.trim();
 
+        let commands = HashSet::from(["echo", "exit", "type"]);
+
+        // I think all of these could be stored within a type of some kind as a switch case
+        // to match against
         if input == "exit" {
             break;
         } else if input.starts_with("echo ") {
             println!("{}", &input[5..]);
+        } else if input.starts_with("type ") {
+            let command = &input[5..];
+            if commands.contains(command) {
+                println!("{command} is a shell builtin");
+            }
         } else {
             println!("{input}: command not found");
         }
