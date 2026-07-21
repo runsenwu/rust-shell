67271b730903a4ea5590a47e6b6fb26b00c40d37
runsenwu <84985987+runsenwu@users.noreply.github.com>
2026-07-20 22:55:43 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use is_executable::IsExecutable;
 use std::fs::{self, Permissions};
+use std::io::ErrorKind::NotFound;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
+use std::process::Command;
+use std::sync::LazyLock;
 use std::{self, collections::HashSet};
-use std::{env, path, string};
+use std::{env, path, println, string};
+
+static COMMANDS: LazyLock<HashSet<&str>> =
+    LazyLock::new(|| HashSet::from(["echo", "exit", "type"]));
 
 fn main() {
     loop {
@@ -21,8 +27,6 @@ fn main() {
 
         let input = input.trim();
 
-        let commands = HashSet::from(["echo", "exit", "type"]);
-
         let dirs: Vec<&str> = vec![];
 
         // I think all of these could be stored within a type of some kind as a switch case
@@ -34,51 +38,78 @@ fn main() {
         } else if input.starts_with("type ") {
             // all of these things can go into functions, instead of all living in main
             let command = &input[5..].trim();
-            if commands.contains(command) {
-                println!("{command} is a shell builtin");
+            let (found, result) = is_executable(command);
+
+            if found {
+                println!("{result}");
             } else {
-                let test = env::var("PATH").unwrap_or_default();
+                command_not_found(command);
+            }
+        } else {
+            let mut splits = input.split_whitespace();
 
-                if test.is_empty() {
-                    println!("{command}: not found")
-                } else {
-                    let paths = env::split_paths(&test);
+            let throw: Vec<&str> = splits.clone().collect();
 
-                    let mut found = false;
+            let command = splits.next().unwrap();
 
-                    // perm checking
-                    for path in paths {
-                        let temp_path = path.to_str().unwrap_or_default();
-                        let path_str = &format!("{}/{command}", temp_path);
-                        let path_with_file = Path::new(path_str);
+            let (found, result) = is_executable(command);
 
-                        if path_with_file.exists() {
-                            let permission = match fs::metadata(&path_with_file) {
-                                Ok(mode) => mode.permissions().mode(),
-                                Err(_) => continue,
-                            };
+            if found {
+                match Command::new(command).args(splits).status() {
+                    Ok(_) => {}
+                    Err(_) => println!("Execution failed"),
+                }
+            } else {
+                command_not_found(command);
+            }
+        }
+    }
+}
 
-                            // if executable
-                            if permission & 0o111 != 0 {
-                                println!(
-                                    "{command} is {}",
-                                    &path_with_file.to_str().unwrap_or_default()
-                                );
+fn command_not_found(command: &str) {
+    println!("{command}: not found");
+}
 
-                                found = true;
+fn is_executable(command: &str) -> (bool, String) {
+    if COMMANDS.contains(command) {
+        return (true, format!("{command} is a shell builtin"));
+    } else {
+        let test = env::var("PATH").unwrap_or_default();
 
-                                break;
-                            }
-                        }
-                    }
+        if test.is_empty() {
+            return (false, String::new());
+        } else {
+            let paths = env::split_paths(&test);
+            let mut ret_string = String::new();
 
-                    if (!found) {
-                        println!("{command}: not found");
+            let mut found = false;
+
+            // perm checking
+            for path in paths {
+                let temp_path = path.to_str().unwrap_or_default();
+                let path_str = &format!("{}/{command}", temp_path);
+                let path_with_file = Path::new(path_str);
+
+                if path_with_file.exists() {
+                    let permission = match fs::metadata(&path_with_file) {
+                        Ok(mode) => mode.permissions().mode(),
+                        Err(_) => continue,
+                    };
+
+                    // if executable
+                    if permission & 0o111 != 0 {
+                        ret_string = format!(
+                            "{command} is {}",
+                            path_with_file.to_str().unwrap_or_default()
+                        );
+                        found = true;
+
+                        break;
                     }
                 }
             }
-        } else {
-            println!("{input}: command not found");
+
+            return (found, ret_string);
         }
     }
 }
